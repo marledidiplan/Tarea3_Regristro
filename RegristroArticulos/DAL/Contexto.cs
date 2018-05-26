b5c6d99aee906d444b1a3e72f52580ac75aaff2d
Marledi Alcenia Diplan Reyes <marledi_diplan@ucne.edu.do>
2018-05-26 00:01:54 -0400

    Ya hice la clase Contexto

---
+++ RegristroArticulos/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using RegristroArticulos.Entidades;
+
+
+namespace RegristroArticulos.DAL
+{   // Agregamos public para que la clase pueda ser encontrada en cualquier codigo del proyecto
+    //y heredamos de DbContext para que EntityFramework pueda funcionar
+    public class Contexto : DbContext
+    {
+        public DbSet<ArticulosProducto> Articulo { get; set; }
+        //base(conStr) para pasar la conexion a la clase base de EntityFramework
+        public Contexto() : base("ConStr")
+        { }
+
+    }
+}
